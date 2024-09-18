ce0eae353e1e4fb48b029a653aff1435616643f7
Joan07MadMax <97638171+Joan07MadMax@users.noreply.github.com>
2024-09-18 17:10:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+song = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightwristX = 0;
+rightwristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
+function preload() 
+{
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet está inicializado');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+    }
+}
